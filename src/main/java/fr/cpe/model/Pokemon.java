e203a66d6e625224da332cad2a2cfa11b9fbb9af
DAO.FD <fd.dao@eleve.leschartreux.net>
2026-04-01 17:06:22 +0200

    Merge Fxml and DB

--- src/main/java/fr/cpe/model/Pokemon.java
+++ src/main/java/fr/cpe/model/Pokemon.java
@@ -1,5 +1,4 @@
 package fr.cpe.model;
-import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.Map;
 
@@ -11,13 +10,13 @@ public class Pokemon {
     private Attaque[] lesattaquesprises;
     private Integer hp;
     private Map<StatType, Integer> stats;
-    private BufferedImage image_face;
-    private BufferedImage image_dos;
-    private BufferedImage sprite;
+    private String image_face;
+    private String image_dos;
+    private String sprite;
     private char description;
     private Abilite ability;
 
-    public Pokemon(int num_Poke, String nom, List<Type> types, Attaque[] lesattaquesdisponibles, Attaque[] lesattaquesprises, int hp, Map<StatType, Integer> stats, String image_face, String image_dos, String sprite, String description, Abilite ability) {
+    public Pokemon(int num_Poke, String nom, List<Type> types, Attaque[] lesattaquesdisponibles, Attaque[] lesattaquesprises, int hp, Map<StatType, Integer> stats, String image_face, String image_dos, String sprite, char description, Abilite ability) {
         Num_Poke = num_Poke;
         this.nom = nom;
         this.types = types;
@@ -91,27 +90,27 @@ public class Pokemon {
         this.stats = stats;
     }
 
-    public BufferedImage getImage_face() {
+    public String getImage_face() {
         return image_face;
     }
 
-    public void setImage_face(BufferedImage image_face) {
+    public void setImage_face(String image_face) {
         this.image_face = image_face;
     }
 
-    public BufferedImage getImage_dos() {
+    public String getImage_dos() {
         return image_dos;
     }
 
-    public void setImage_dos(BufferedImage image_dos) {
+    public void setImage_dos(String image_dos) {
         this.image_dos = image_dos;
     }
 
-    public BufferedImage getSprite() {
+    public String getSprite() {
         return sprite;
     }
 
-    public void setSprite(BufferedImage sprite) {
+    public void setSprite(String sprite) {
         this.sprite = sprite;
     }
 
