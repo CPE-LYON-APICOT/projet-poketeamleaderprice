cebf39c4f4d18e5dbcec9fb98e91779a1f543d69
SpeeDelta <francoisdanieldao@gmail.com>
2026-05-04 18:14:50 +0200

    Test connexion part 1/?

--- src/main/java/fr/cpe/Controller/ChooseItemsController.java
+++ src/main/java/fr/cpe/Controller/ChooseItemsController.java
@@ -12,6 +12,7 @@ import fr.cpe.model.Item;
 import fr.cpe.model.StatType;
 import fr.cpe.model.Stade;
 import fr.cpe.service.ConnectionService;
+import fr.cpe.service.Partie;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -289,6 +290,10 @@ public class ChooseItemsController {
             this.dresseur.setIndex(0);
             connectionService.hostGame(this.dresseur, stade);
 
+            // Remplir immédiatement l'instance Partie en local pour éviter les blocages réseau
+            Partie.getInstance().setDresseur1(this.dresseur);
+            Partie.getInstance().setStade(stade);
+
             navigateToChargement(event);
         } catch (Exception e) {
             e.printStackTrace();
@@ -300,6 +305,9 @@ public class ChooseItemsController {
             ensureConnectionService();
             this.dresseur.setIndex(1);
             connectionService.connect(this.dresseur);
+
+            // Remplir immédiatement l'instance Partie en local pour éviter les blocages réseau
+            Partie.getInstance().setDresseur2(this.dresseur);
 
             navigateToChargement(event);
         } catch (Exception e) {
